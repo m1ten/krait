673ad7e647b7bf79784f8432d8338a38868bfc05
miten <57693631+m1ten@users.noreply.github.com>
2021-10-22 11:19:57 -0400

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,40 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
+#![allow(unreachable_code)]
 
 use dash;
 
 fn main() {
+
+    let strtoml = "
+    
+[info]
+name = 'dash'
+version = '0.1.0'
+author = 'none'
+description = 'dash your way through OS post-install'
+
+[system]
+os = 'Linux'
+distro = 'Arch Linux'
+pkg_mgr = 'pacman'
+pkgs = [ 'discord', 'chrome' ]
+dotfiles = [ '.zshrc', '.vimrc' ]
+
+[[scripts]]
+run = '*pkg_mgr -Syu'
+with = 'root'
+without = 'verbose'
+
+[[scripts]]
+run = 'echo *name'
+with = 'verbose'
+without = 'root'
+    
+    ".to_string();
+
+    return;
+
     // let args = std::env::args().collect();
 
     // basic info about dash, stored into info
@@ -18,6 +49,7 @@ fn main() {
     let clap = dash::Arguments::run(info);
 
     let mut data = "
+
 info:
     name: &name dash
     version: 0.1.0
@@ -34,9 +66,15 @@ system:
     dotfiles:
         - .zshrc
         - .vimrc
-    run:
-        - sudo *pkg_mgr -Syu
-        - echo *name 
+
+scripts:
+    - run: *pkg_mgr -Syu
+      with: root
+      without: verbose
+    - run: echo *name 
+      with: verbose
+      without: root
+
     "
     .to_string();
 
@@ -45,17 +83,17 @@ system:
             Some(s) => s,
             None => break
         };
-        let mut asterisk_end_byte = asterisk_start_byte + 1;
+        let mut asterisk_end_byte = asterisk_start_byte;
 
         for i in data[asterisk_start_byte..].to_string().chars() {
-            if i == ' ' {
-                break;
-            } else {
+            if i != ' ' {
                 asterisk_end_byte += 1;
+            } else {
+                break;
             }
         }
 
-        let asterisk = &data[asterisk_start_byte..asterisk_end_byte - 1];
+        let asterisk = &data[asterisk_start_byte..asterisk_end_byte];
 
         let start_byte = match data.find(&"&{}".replace("{}", &asterisk.replace("*", ""))) {
             Some(s) => s,
@@ -65,18 +103,18 @@ system:
         let mut end_byte = start_byte + 1;
 
         for i in data[start_byte..].to_string().chars() {
-            if i == ' ' {
-                break;
-            } else {
+            if i != ' ' {
                 middle_byte += 1;
+            } else {
+                break;
             }
         }
         
         for i in data[start_byte..].to_string().chars() {
-            if i == '\n' {
-                break;
-            } else {
+            if i != '\n' {
                 end_byte += 1;
+            } else {
+                break;
             }
         }
 
@@ -86,54 +124,10 @@ system:
         data = data.replace(asterisk, &result.replace("\n", ""));
     }
 
-    let parsed: serde_yaml::Value = serde_yaml::from_str(data.as_str()).unwrap();
+    println!("{}", data);
 
-    println!("{:?}", parsed);
-    // println!("{}", data);
-
-    // println!("{:#?}", parsed);
-
-    // let mut parsed = data.parse::<toml::Value>().unwrap();
-
-    // loop {
-    //     let start_bytes = data.find("{").unwrap_or(0);
-    //     let end_bytes = data.find("}").unwrap_or(0);
-
-    //     if start_bytes == 0 || end_bytes == 0 {
-    //         break;
-    //     }
-
-    //     let result = data[start_bytes..end_bytes + 1].to_string();
-    //     let clean_result = &result[1..&result.len() - 1]
-    //         .split('.')
-    //         .collect::<Vec<&str>>();
-
-    //     if !parsed[clean_result[0]][clean_result[1]].is_str() {
-    //         data = data.replace(
-    //             result.as_str(),
-    //             "{0}.{1}"
-    //                 .replace("{0}", clean_result[0])
-    //                 .as_str()
-    //                 .replace("{1}", clean_result[1])
-    //                 .as_str(),
-    //         );
-    //     } else {
-    //         data = data.replace(
-    //             result.as_str(),
-    //             parsed[clean_result[0]][clean_result[1]].as_str().unwrap(),
-    //         );
-    //     }
-    // }
-
-    // parsed = data.parse::<toml::Value>().unwrap();
-
-    // if parsed.get("info").is_some() && parsed["info"].is_table()
-    // //    || parsed.get("information").is_some() && parsed["information"].is_table()
-    // {
-    //     for (i, j) in info {
-    //         if parsed["info"].get(i).is_none() {
-    //             parsed["info"]["name"] = toml::Value::String(i.to_string());
-    //         }
-    //     }
-    // }
+    let parsed = match serde_yaml::from_str::<serde_yaml::Value>(data.as_str()) {
+        Ok(k) => k,
+        Err(e) => serde_yaml::Value::String(e.to_string())
+    };
 }
