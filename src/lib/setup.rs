859168a0496837c4b749fd2d07a4291ccad0abc6
miten <57693631+m1ten@users.noreply.github.com>
2021-11-09 17:33:55 -0500

    Update setup.rs

--- src/lib/setup.rs
+++ src/lib/setup.rs
@@ -5,11 +5,16 @@ pub fn run(info: wix::structs::Information, args: wix::args::Arguments) {
 
     // rewrite these functions because they suck and won't work on linux.
     if is_super() {
-        panic!("You are not allowed to run this program as root.");
+        eprintln!("{}", "Error: You are running wix as root.");
+        std::process::exit(1);
     }
+
     if !is_python_installed() {
-        panic!("Python is not installed.");
+        eprintln!("Error: Python is not installed.");
+        std::process::exit(1);
     }
+
+    println!("works");
 }
 
 // function to check if running as root/admin
@@ -34,9 +39,8 @@ fn is_super() -> bool {
 
 // function to check if python is installed
 fn is_python_installed() -> bool {
-    if which::which("python3").is_ok() || which::which("python").is_ok() {
-        return true;
-    } else {
-        return false;
+    match which::which("python3") {
+        Ok(_) => true,
+        Err(_) => false,
     }
 }
