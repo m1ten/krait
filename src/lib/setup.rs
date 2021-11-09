3cadbb51fd25d9997ed978f01d6190884d756975
miten <57693631+m1ten@users.noreply.github.com>
2021-11-09 16:52:04 -0500

    better again

--- src/lib/setup.rs
+++ src/lib/setup.rs
@@ -4,102 +4,41 @@ pub fn run(info: wix::structs::Information, args: wix::args::Arguments) {
     // TODO: Implement setup.rs
 
     // rewrite these functions because they suck and won't work on linux.
-    is_super();
-    is_python_installed();
+    if is_super() {
+        panic!("You are not allowed to run this program as root.");
+    }
+    if !is_python_installed() {
+        panic!("Python is not installed.");
+    }
+
+    #[cfg(target_os = "linux")] 
+    { 
+        let distro = sysinfo.distro_name();
+        println!("{}", distro);
+    }
 }
 
 // function to check if running as root/admin
 fn is_super() -> bool {
-    if cfg!(target_os = "macos") {
-        let output = std::process::Command::new("id")
-            .arg("-g")
-            .output()
-            .expect("failed to execute process");
-        let output = String::from_utf8_lossy(&output.stdout);
-        if output.trim() == "0" {
-            return true;
-        } else {
-            return false;
-        }
-
-    } else if cfg!(target_os = "linux") {
-        let output = std::process::Command::new("id")
-            .arg("-g")
-            .output()
-            .expect("failed to execute process");
-        let output = String::from_utf8_lossy(&output.stdout);
-        if output.trim() == "0" {
-            return true;
-        } else {
-            return false;
-        }
-
-    } else if cfg!(target_os = "windows") {
+    if cfg!(windows) {
         let output = std::process::Command::new("powershell")
             .arg("-Command")
-            .arg("Get-WmiObject -Class Win32_UserAccount -Filter \"LocalAccount=TRUE\" -Property Name")
+            .arg("Get-WmiObject -Class Win32_UserAccount -Filter \"Name='Administrator'\" -Property LocalAccount")
             .output()
-            .expect("failed to execute process");
-        let output = String::from_utf8_lossy(&output.stdout);
-        if output.trim() == "Administrator" {
-            return true;
-        } else {
-            return false;
-        }
+            .expect("Failed to execute command");
 
+        let stdout = String::from_utf8_lossy(&output.stdout);
+        stdout.contains("True")
     } else {
-        panic!("Unsupported OS");
+        nix::unistd::getuid().is_root()
     }
 }
 
 // function to check if python is installed
 fn is_python_installed() -> bool {
-    if cfg!(target_os = "macos") {
-        let output = std::process::Command::new("which")
-            .arg("python3")
-            .output()
-            .expect("failed to execute process");
-        let output = String::from_utf8_lossy(&output.stdout);
-        if output.trim() == "/usr/bin/python3" {
-            return true;
-        } else {
-            return false;
-        }
-    } else if cfg!(target_os = "linux") {
-        let output = std::process::Command::new("which")
-            .arg("python3")
-            .output()
-            .expect("failed to execute process");
-        let output = String::from_utf8_lossy(&output.stdout);
-        if output.trim() == "/usr/bin/python3" {
-            return true;
-        } else {
-            return false;
-        }
-    } else if cfg!(target_os = "windows") {
-        let output = std::process::Command::new("powershell")
-            .arg("-Command")
-            .arg("Get-WmiObject -Class Win32_Product -Filter \"Name='Python'\" -Property Name")
-            .output()
-            .expect("failed to execute process");
-        let output = String::from_utf8_lossy(&output.stdout);
-        if output.trim() == "Python" {
-            return true;
-        } else {
-            return false;
-        }
+    if cfg!(windows) {
+        which::which("python3").is_ok()
     } else {
-        panic!("Unsupported OS");
+        which::which("python").is_ok()
     }
-}
-
-// function to get linux distro
-#[cfg(target_os = "linux")]
-fn get_linux_distro() -> String {
-    let output = std::process::Command::new("cat")
-        .arg("/etc/os-release")
-        .output()
-        .expect("failed to execute process");
-    let output = String::from_utf8_lossy(&output.stdout);
-    output
 }
