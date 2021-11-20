68c225140b1652561f834bca6594aeebff38a05d
miten <57693631+m1ten@users.noreply.github.com>
2021-11-20 14:59:40 -0500

    Update setup.rs

--- src/lib/setup.rs
+++ src/lib/setup.rs
@@ -1,3 +1,5 @@
+use std::{process::Command, vec};
+
 use crate as wix;
 
 pub fn run(info: wix::structs::Information, args: wix::args::Arguments) {
@@ -9,7 +11,7 @@ pub fn run(info: wix::structs::Information, args: wix::args::Arguments) {
 // function to check if running as root/admin
 pub fn is_super() -> bool {
     #[cfg(windows)]
-    {        
+    {
         is_elevated::is_elevated()
     }
 
@@ -21,8 +23,19 @@ pub fn is_super() -> bool {
 
 // function to check if python is installed
 pub fn is_python_installed() -> bool {
-    match which::which("python") {
-        Ok(_) => true,
-        Err(_) => false,
+    let name: Vec<&str> = vec!["py","python", "python3", "pypy", "pypy3"];
+    let version: Vec<&str> = vec!["3.8", "3.9", "3.10"];
+    for i in name.iter() {
+        for j in version.iter() {
+            let output = match Command::new(*i).arg("--version").output() {
+                Ok(o) => o,
+                Err(_) => return false
+            };
+            let output = String::from_utf8_lossy(&output.stdout).to_string();
+            if output.contains(j) {
+                return true;
+            }
+        }
     }
+    false
 }
